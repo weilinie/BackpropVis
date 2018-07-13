fa8813e883247c511fe0d3cba9b21fc9a12b2788
Will Nie <monde.nie@gmail.com>
2018-07-12 23:41:12 -0700

    init commit

---
+++ convnet/convnet.py
@@ -0,0 +1,77 @@
+import numpy as np
+import tensorflow as tf
+
+
+class Convnet(object):
+    def __init__(self, sess=None):
+
+        self.layers_dic = {}
+        self.parameters = []
+
+        self.n_labels = 1000
+        self.n_input = 224
+
+        # zero-mean input
+        with tf.name_scope('input') as scope:
+            self.images = tf.placeholder(tf.float32, [None, self.n_input, self.n_input, 3])
+            mean = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
+            self.imgs = self.images - mean
+            self.layers_dic['imgs'] = self.imgs
+
+        with tf.name_scope('output') as scope:
+            self.labels = tf.placeholder(tf.float32, [None, self.n_labels])
+
+        self.convlayers()
+        self.fc_layers()
+
+        self.logits = self.fc2l
+
+        self.probs = tf.nn.softmax(self.logits)
+        self.cost = tf.reduce_sum((self.probs - self.labels) ** 2)
+        self.maxlogit = tf.reduce_max(self.logits, axis=1)
+
+        if sess is not None:
+            self.init(sess)
+        else:
+            print("convnet initialization failed ... ")
+
+    def convlayers(self):
+
+        conv_ch1 = 16  # adjust this value to control the visual quality of visualizations
+
+        # conv1
+        with tf.name_scope('conv1_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([7, 7, 3, conv_ch1], dtype=tf.float32,
+                                                     stddev=1e-1), name='weights')
+            conv = tf.nn.conv2d(self.imgs, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[conv_ch1], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv1 = tf.nn.relu(out, name=scope)
+
+            self.parameters += [kernel, biases]
+            self.layers_dic['conv1_1'] = self.conv1
+
+        self.convnet_out = self.conv1
+
+    def fc_layers(self):
+
+        shape = int(np.prod(self.convnet_out.get_shape()[1:]))
+        self.conv_flat = tf.reshape(self.convnet_out, [-1, shape])
+
+        flat_len = self.conv_flat.get_shape().as_list()[1]
+
+        # fc2
+        with tf.name_scope('fc2') as scope:
+            fc2w = tf.Variable(tf.truncated_normal([flat_len, self.n_labels],
+                                                   dtype=tf.float32,
+                                                   stddev=1e-1), name='weights')
+            fc2b = tf.Variable(tf.constant(0.0, shape=[self.n_labels], dtype=tf.float32),
+                               trainable=True, name='biases')
+            self.fc2l = tf.nn.bias_add(tf.matmul(self.conv_flat, fc2w), fc2b)
+
+            self.parameters += [fc2w, fc2b]
+            self.layers_dic['fc2'] = self.fc2l
+
+    def init(self, sess):
+        sess.run(tf.global_variables_initializer())
---
+++ convnet/visualize_convnet.py
@@ -0,0 +1,91 @@
+from scipy.misc import imread, imresize
+import os
+import numpy as np
+import random
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import gen_nn_ops
+import glob
+
+from convnet import Convnet
+from utils import print_prob, visualize
+
+image_dict = {'tabby': 281, 'laska': 356, 'mastiff': 243, 'panda': 388}
+
+
+@ops.RegisterGradient("GuidedRelu")
+def _GuidedReluGrad(op, grad):
+    return tf.where(0. < grad, gen_nn_ops._relu_grad(grad, op.outputs[0]), tf.zeros(tf.shape(grad)))
+
+
+def visualize_convnet():
+    sal_map_type = "GuidedBackprop_maxlogit"  # change it to get different visualizations
+    image_name = 'tabby'  # or using a list to deal with multiple images
+
+    data_dir = "../data"
+    save_dir = "results"
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    n_labels = 1000
+    n_input = 224
+
+    fns = []
+    image_list = []
+    label_list = []
+
+    # load in the original image and its adversarial examples
+    for image_path in glob.glob(os.path.join(data_dir, '{}.png'.format(image_name))):
+        fns.append(os.path.basename(image_path).split('.')[0])
+        image = imread(image_path, mode='RGB')
+        image = imresize(image, (n_input, n_input)).astype(np.float32)
+        image_list.append(image)
+        onehot_label = np.array([1 if i == image_dict[image_name] else 0 for i in range(n_labels)])
+        label_list.append(onehot_label)
+
+    batch_img = np.array(image_list)
+    batch_label = np.array(label_list)
+
+    batch_size = batch_img.shape[0]
+
+    # tf session
+    sess = tf.Session()
+
+    # construct the graph based on the gradient type we want
+    # plain relu vs guidedrelu
+    if sal_map_type.split('_')[0] == 'GuidedBackprop':
+        eval_graph = tf.get_default_graph()
+        with eval_graph.gradient_override_map({'Relu': 'GuidedRelu'}):
+            conv_model = Convnet(sess)
+
+    elif sal_map_type.split('_')[0] == 'PlainSaliency':
+        conv_model = Convnet(sess)
+
+    else:
+        raise Exception("Unknown saliency type")
+
+    # saliency gradient to input layer
+    if sal_map_type.split('_')[1] == "cost":
+        sal_map = tf.gradients(conv_model.cost, conv_model.imgs)[0]
+    elif sal_map_type.split('_')[1] == 'maxlogit':
+        sal_map = tf.gradients(conv_model.maxlogit, conv_model.imgs)[0]
+    elif sal_map_type.split('_')[1] == 'randlogit':
+        sal_map = tf.gradients(conv_model.logits[random.randint(0, 999)], conv_model.imgs)[0]
+    else:
+        raise Exception("Unknown logit gradient type")
+
+    # predict
+    probs = sess.run(conv_model.probs, feed_dict={conv_model.images: batch_img})
+
+    # sal_map
+    sal_map_val = sess.run(sal_map, feed_dict={conv_model.images: batch_img, conv_model.labels: batch_label})
+
+    for idx in range(batch_size):
+        print_prob(probs[idx])
+        visualize(sal_map_val[idx], sal_map_type, save_dir, fns[idx])
+
+
+if __name__ == '__main__':
+    # setup the GPUs to use
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    visualize_convnet()
